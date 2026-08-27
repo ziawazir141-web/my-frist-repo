54e3e0c2492030137b23223481ac9747ec9d86cc
Zia ur Rehman <ziawazir141@gmail.com>
2026-08-27 01:51:00 -0700

    zia

---
+++ x=10.py
@@ -0,0 +1,22 @@
+'''x=10
+y=15
+
+z= x*y
+print(z)
+
+print (x==y)
+'''
+#slicing
+#x = 'PAKISTAN'
+#rint(x[2:4])
+"""
+x=15
+y=29
+print(x == y)
+"""
+x = 888888 x = 88888
+y = 8888
+
+print (x)
+print(x)
+print(y)
